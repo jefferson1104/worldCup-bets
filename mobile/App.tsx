2b2a599b4c9179299507e8045fe831a8351b06a7
Jefferson Soares <jefferson1104junior@gmail.com>
2022-11-02 13:24:56 -0300

    create authentication interface

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,9 @@
 import { NativeBaseProvider, StatusBar } from "native-base";
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-import { SignIn } from './src/screens/SignIn';
 import { Loading } from './src/components/Loading';
+
+import { SignIn } from './src/screens/SignIn';
 
 import { THEME } from './src/styles/theme';
 
--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -9,14 +9,12 @@ export function SignIn() {
   return (
     <Center flex={1} bgColor="gray.900" p={7}>
       <Logo width={212} height={40} />
-
       <Button
         title="ENTRAR COM GOOGLE"
         leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
         type="SECONDARY"
         mt={12}
       />
-
       <Text color="white" textAlign="center" mt={4}>
         Não utilizamos nenhuma informação além {'\n'}
         do seu e-mail para criação de sua conta.
