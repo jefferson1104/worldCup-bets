88b0dc5347830a8c510a321b660c38a21468ac35
Jefferson Soares <jefferson1104junior@gmail.com>
2022-11-02 22:39:00 -0300

    create page Find

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -7,6 +7,7 @@ import { Loading } from './src/components/Loading';
 
 import { SignIn } from './src/screens/SignIn';
 import { New } from './src/screens/New';
+import { Find } from './src/screens/Find';
 
 import { THEME } from './src/styles/theme';
 
@@ -21,7 +22,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        {fontsLoaded ? <New /> : <Loading />}
+        {fontsLoaded ? <Find /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
