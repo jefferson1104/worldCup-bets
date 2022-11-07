2358a690749f9582741808170c557833e50c1f07
Jefferson Soares <jefferson1104junior@gmail.com>
2022-11-07 05:20:20 -0300

    separating files, creating user, generating JWT, validating JWT

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,6 +1,71 @@
 import { FastifyInstance } from 'fastify';
+import { z } from 'zod';
 import { prisma } from '../lib/prisma';
+import { authenticate } from '../plugins/authenticate';
 
-export function authRoutes(fastify: FastifyInstance) {
+export async function authRoutes(fastify: FastifyInstance) {
+  fastify.get('/me', { onRequest: [authenticate] }, async (request) => {
+    return { user: request.user }
+  });
 
+  fastify.post('/users', async (request) => {
+    const creteUserBody = z.object({
+      access_token: z.string(),
+    });
+
+    // access token received via payload
+    const { access_token } = creteUserBody.parse(request.body);
+
+    // fetch data from google apis
+    const userResponse = await fetch('https://www.googleapis.com/oauth2/v2/userinfo', {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${access_token}`
+      }
+    });
+
+    // user data that came from google api request
+    const userData = await userResponse.json();
+
+    // schema to validate userData
+    const userInfoSchema = z.object({
+      id: z.string(),
+      email: z.string().email(),
+      name: z.string(),
+      picture: z.string().url(),
+    });
+
+    // data validated
+    const userInfo = userInfoSchema.parse(userData);
+
+    // find user where googleId is equal id
+    let user = await prisma.user.findUnique({
+      where: {
+        googleId: userInfo.id
+      }
+    });
+
+    // create user if you can't find a user with googleId
+    if (!user) {
+      user = await prisma.user.create({
+        data: {
+          googleId: userInfo.id,
+          name: userInfo.name,
+          email: userInfo.email,
+          avatarUrl: userInfo.picture
+        }
+      });
+    }
+
+    // create JWT token
+    const token = fastify.jwt.sign({
+      name: user.name,
+      avatarUrl: user.avatarUrl,
+    }, {
+      sub: user.id,
+      expiresIn: '7 days',
+    });
+
+    return { token }
+  });
 }
--- server/src/routes/game.ts
+++ server/src/routes/game.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from 'fastify';
 import { prisma } from '../lib/prisma';
 
-export function gameRoutes(fastify: FastifyInstance) {
+export async function gameRoutes(fastify: FastifyInstance) {
 
 }
